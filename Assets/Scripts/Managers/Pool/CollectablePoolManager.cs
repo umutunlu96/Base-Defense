610ff3e3d4e900101c4f0db763028a142e88121a
umutunlu96 <umutt.unlu.96@gmail.com>
2022-09-15 15:13:50 +0300

    EnemyPool Manager & Collectables Pool Manager implemented

---
+++ Assets/Scripts/Managers/Pool/CollectablePoolManager.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using Extentions;
+using UnityEngine;
+
+namespace Managers.Pool
+{
+    public class CollectablePoolManager : MonoBehaviour
+    {
+        public List<GameObject> MoneyList;
+        public List<GameObject> GemList;
+        
+        [SerializeField] private GameObject moneyPrefab;
+        [SerializeField] private GameObject gemPrefab;
+        
+        public static ObjectPool<GameObject> MoneyPool;
+        public static ObjectPool<GameObject> GemPool;
+
+
+        public int MoneyAmount = 5;
+        public int GemAmount = 5;
+
+        private void Awake()
+        {
+            InitPool();
+        }
+
+        private void InitPool()
+        {
+            MoneyPool = new ObjectPool<GameObject>(MoneyFactory,MoneyTurnOnCallback,MoneyTurnOffCallback,MoneyAmount,true);
+            GemPool = new ObjectPool<GameObject>(GemFactory, GemTurnOnCallback, GemTurnOffCallback, GemAmount, true);
+        }
+
+        private GameObject MoneyFactory()
+        {
+            GameObject moneyObject = Instantiate(moneyPrefab, transform);
+            return moneyObject;
+        }
+
+        private void MoneyTurnOnCallback(GameObject money)
+        {
+            MoneyList.Remove(money);
+            MoneyList.TrimExcess();
+            money.SetActive(true);
+        }
+        
+        private void MoneyTurnOffCallback(GameObject money)
+        {
+            MoneyList.Add(money);
+            MoneyList.TrimExcess();
+            money.SetActive(false);
+        }
+        
+        private GameObject GemFactory()
+        {
+            GameObject gemObject = Instantiate(gemPrefab, transform);
+            return gemObject;
+        }
+        
+        private void GemTurnOnCallback(GameObject gem)
+        {
+            GemList.Remove(gem);
+            GemList.TrimExcess();
+            gem.SetActive(true);
+        }
+        
+        private void GemTurnOffCallback(GameObject gem)
+        {
+            GemList.Add(gem);
+            GemList.TrimExcess();
+            gem.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/Managers/Pool/EnemyPoolManager.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using Extentions;
+using UnityEngine;
+
+namespace Managers.Pool
+{
+    public class EnemyPoolManager : MonoBehaviour
+    {
+        public List<GameObject> RedEnemyList;
+        public List<GameObject> OrangeEnemyList;
+        public List<GameObject> BigRedEnemyList;
+
+        [SerializeField] private GameObject redEnemyPrefab;
+        [SerializeField] private GameObject orangeEnemyPrefab;
+        [SerializeField] private GameObject bigRedEnemyPrefab;
+
+        public static ObjectPool<GameObject> RedEnemyPool;
+        public static ObjectPool<GameObject> OrangeEnemyPool;
+        public static ObjectPool<GameObject> BigRedEnemyPool;
+
+        public int RedEnemyCount;
+        public int OrangeEnemyCount;
+        public int BigRedEnemyCount;
+
+
+        private void Awake()
+        {
+            InitPool();
+        }
+
+        private void InitPool()
+        {
+            RedEnemyPool = new ObjectPool<GameObject>(RedEnemyFactory, RedEnemyTurnOnCallback, RedEnemyTurnOffCallback,
+                RedEnemyCount, true);
+            OrangeEnemyPool = new ObjectPool<GameObject>(OrangeEnemyFactory, OrangeEnemyTurnOnCallback,
+                OrangeEnemyTurnOffCallback, OrangeEnemyCount, true);
+            BigRedEnemyPool = new ObjectPool<GameObject>(BigRedEnemyFactory, BigRedEnemyTurnOnCallback,
+                BigRedEnemyTurnOffCallback, BigRedEnemyCount, true);
+        }
+
+        #region RedEnemyFactory
+        
+        private GameObject RedEnemyFactory()
+        {
+            GameObject redEnemyObject = Instantiate(redEnemyPrefab, transform);
+            return redEnemyObject;
+        }
+
+        private void RedEnemyTurnOnCallback(GameObject enemy)
+        {
+            RedEnemyList.Remove(enemy);
+            RedEnemyList.TrimExcess();
+            enemy.SetActive(true);
+        }
+        
+        private void RedEnemyTurnOffCallback(GameObject enemy)
+        {
+            RedEnemyList.Add(enemy);
+            RedEnemyList.TrimExcess();
+            enemy.SetActive(false);
+        }
+        
+        #endregion
+
+        #region OrangeEnemyFactory
+        
+        private GameObject OrangeEnemyFactory()
+        {
+            GameObject orangeEnemyObject = Instantiate(orangeEnemyPrefab, transform);
+            return orangeEnemyObject;
+        }
+
+        private void OrangeEnemyTurnOnCallback(GameObject enemy)
+        {
+            OrangeEnemyList.Remove(enemy);
+            OrangeEnemyList.TrimExcess();
+            enemy.SetActive(true);
+        }
+        
+        private void OrangeEnemyTurnOffCallback(GameObject enemy)
+        {
+            OrangeEnemyList.Add(enemy);
+            OrangeEnemyList.TrimExcess();
+            enemy.SetActive(false);
+        }
+        
+        #endregion
+        
+        #region BigRedEnemyFactory
+        
+        private GameObject BigRedEnemyFactory()
+        {
+            GameObject bigRedEnemyObject = Instantiate(bigRedEnemyPrefab, transform);
+            return bigRedEnemyObject;
+        }
+
+        private void BigRedEnemyTurnOnCallback(GameObject enemy)
+        {
+            BigRedEnemyList.Remove(enemy);
+            BigRedEnemyList.TrimExcess();
+            enemy.SetActive(true);
+        }
+        
+        private void BigRedEnemyTurnOffCallback(GameObject enemy)
+        {
+            BigRedEnemyList.Add(enemy);
+            BigRedEnemyList.TrimExcess();
+            enemy.SetActive(false);
+        }
+        
+        #endregion
+    }
+}
